05f5521a59df7b79d9e273e9b5db953defee39e0
lrpv2005-code <lrpv2005@gmail.com>
2026-02-11 19:37:49 -0400

    Delete apps.py

--- apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class TransaccionesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transacciones'
-
-    def ready(self):
-        import transacciones.signals
